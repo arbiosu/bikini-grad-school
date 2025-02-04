9dbd609f86460d1679fc2779e2e9f2f348800508
Arberim Ame <amea@oregonstate.edu>
2025-02-04 18:57:34 -0500

    Add supabase

---
+++ project/src/lib/supabase/service.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js"
+import { Database } from "@/lib/supabase/database"
+
+/**
+ * Supabase client for database services, only used server side.
+ * @returns 
+ */
+export async function createServiceClient() {
+    return createClient<Database>(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.SUPABASE_SERVICE_KEY!,
+    )
+  }
