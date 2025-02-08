c53cc202e48170cd837beb92739098e6cafa603b
Arberim Ame <amea@oregonstate.edu>
2025-02-07 21:52:02 -0500

    Protect page

--- project/src/app/admin/create/page.tsx
+++ project/src/app/admin/create/page.tsx
@@ -1,5 +1,15 @@
 import { UploadArticle } from "@/components/Admin"
+import { redirect } from "next/navigation"
+import { createClient } from "@/lib/supabase/server"
 
-export default function Page() {
+export default async function Page() {
+    const supabase = await createClient()
+    const { data, error } = await supabase.auth.getUser()
+
+    if (error || !data.user) {
+        console.log('Auth Error:', error)
+        redirect('/admin/login')
+    }
+
     return <UploadArticle />
 }
