24f627d5c2dc5115578b6ac5c3014b1ed926aaa6
Arberim Ame <amea@oregonstate.edu>
2026-02-14 03:29:29 -0500

    add cloudflare image loader

---
+++ src/lib/image-loader.ts
@@ -0,0 +1,20 @@
+import type { ImageLoaderProps } from 'next/image';
+
+const normalizeSrc = (src: string) => {
+  return src.startsWith('/') ? src.slice(1) : src;
+};
+
+export default function cloudflareLoader({
+  src,
+  width,
+  quality,
+}: ImageLoaderProps) {
+  let ASSET_URL = 'https://bikinigradschoolassets.com';
+  if (process.env.NODE_ENV === 'development') {
+    ASSET_URL = 'https://pub-2e58def0a2d64e1992d76e72a67be0ee.r2.dev';
+    return `${ASSET_URL}/${normalizeSrc(src)}`;
+  }
+
+  const params = [`width=${width}`, `quality=${quality || 75}`];
+  return `${ASSET_URL}/cdn-cgi/image/${params.join(',')}/${normalizeSrc(src)}`;
+}
